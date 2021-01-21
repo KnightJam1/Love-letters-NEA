9d54d5a34686aedd424d3bcf967d220dd9b39c63
KnightJam1 <70148645+KnightJam1@users.noreply.github.com>
2021-01-21 15:06:21 +0000

    Uploaded Assets

---
+++ assets/Scripts/GameScript.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Manager;
+
+public class GameScript : MonoBehaviour
+{
+    // Opening the text files and putting them in a TextAsset object
+    public TextAsset maleNameFile;
+    public TextAsset femaleNameFile;
+    public TextAsset prefixFile;
+    public TextAsset surnameFile;
+    public TextAsset suffixFile;
+
+    private string[] maleNames;
+    private string[] femaleNames;
+    private string[] prefixes;
+    private string[] surnames;
+    private string[] suffixes;
+
+    // MaleNameGen returns a male forename
+    private string maleNameGen()
+    {
+        return(maleNames[Random.Range(0,maleNames.Length)]);
+    }
+
+    // FemaleNameGen returns a female forename
+    private string femaleNameGen()
+    {
+        return(femaleNames[Random.Range(0,femaleNames.Length)]);
+    }
+
+    // SurnameGen returns a random surname
+    private string surnameGen()
+    {
+        int variety = Random.Range(0,9);
+        switch (variety)
+        {
+            case 0:
+                return(prefixes[Random.Range(0,prefixes.Length)] + surnames[Random.Range(0,surnames.Length)] + suffixes[Random.Range(0,suffixes.Length)]);
+            case 1:
+                return(prefixes[Random.Range(0,prefixes.Length)] + surnames[Random.Range(0,surnames.Length)]);
+            case 2:
+                return(surnames[Random.Range(0,surnames.Length)] + suffixes[Random.Range(0,suffixes.Length)]);
+            default:
+                return(surnames[Random.Range(0,surnames.Length)]);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Collect all names from the text files
+        maleNames = maleNameFile.text.Split('\n');
+        femaleNames = femaleNameFile.text.Split('\n');
+        prefixes = prefixFile.text.Split('\n');
+        surnames = surnameFile.text.Split('\n');
+        suffixes = suffixFile.text.Split('\n');
+        string royalLine = surnameGen();
+
+        // Create the empty deck
+        GroupedCards deck = new GroupedCards();
+        Debug.Log("Created Deck");
+
+        // Put 5 guard cards into the deck
+        for (int i = 0; i < 5; i++)
+        {
+            if (Random.Range(0,2) == 1)
+            {
+                deck.addCard(new GuardCard(maleNameGen() + " " + surnameGen()));
+            }
+            else
+            {
+                deck.addCard(new GuardCard(femaleNameGen() + " " + surnameGen()));
+            }
+            Debug.Log("Added Guard " + (i+1) + ": " + deck.getCard(i).getCardName() + " to deck.");
+        }
+
+        // Put 2 priest cards into the deck
+        for (int i = 0; i < 2; i++)
+        {
+            deck.addCard(new PriestCard(maleNameGen() + " " + surnameGen()));
+            Debug.Log("Added Priest " + (i+1)+ ": " + deck.getCard(5+i).getCardName() + " to deck.");
+        }
+
+        // Put 2 baron cards into the deck
+        for (int i = 0; i < 2; i++)
+        {
+            deck.addCard(new BaronCard(maleNameGen() + " " + surnameGen()));
+            Debug.Log("Added Baron " + (i+1)+ ": " + deck.getCard(7 + i).getCardName() + " to deck.");
+        }
+
+        // Put 2 handmaid cards into the deck
+        for (int i = 0; i < 2; i++)
+        {
+            deck.addCard(new HandmaidCard(femaleNameGen() + " " + surnameGen()));
+            Debug.Log("Added Handmaid " + (i+1)+ ": " + deck.getCard(9+i).getCardName() + " to deck.");
+        }
+        
+        // Put 2 prince cards into the deck
+        for (int i = 0; i < 2; i++)
+        {
+            deck.addCard(new PrinceCard(maleNameGen() + " " + royalLine));
+            Debug.Log("Added Prince " + (i+1)+ ": " + deck.getCard(11 + i).getCardName() + " to deck.");
+        }
+        
+        // Put king into deck
+        deck.addCard(new KingCard(maleNameGen() + " " + royalLine));
+        Debug.Log("Added King: " + deck.getCard(13).getCardName() + " to deck.");
+
+        // Put Countess into deck
+        deck.addCard(new CountessCard(femaleNameGen() + " " + surnameGen()));
+        Debug.Log("Added Countess: " + deck.getCard(14).getCardName() + " to deck.");
+
+        // Put Princess into deck
+        deck.addCard(new PrincessCard(femaleNameGen() + " " + royalLine));
+        Debug.Log("Added Princess: " + deck.getCard(15).getCardName() + " to deck.");
+
+        List<Player> players = new List<Player>();
+        for (int i = 0; i < 4; i++){
+            players.Add(new Player(i, "Player" + (i+1)));
+            Debug.Log("Added new player " + players[i].getPlayerName());
+        }
+
+        GroupedCards hiddenCard = new GroupedCards();
+        hiddenCard.addCard(deck.takeCard(Random.Range(0,deck.getCount())));
+        Debug.Log("Hid " + hiddenCard.getCard(0).getCardRole() + " card");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ assets/Scripts/ManagerScript.cs
@@ -0,0 +1,205 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Manager
+{
+
+    public class Card
+    {
+        protected int value;
+        protected string cardName;
+        protected string role;
+        protected string description;
+        public int getValue()
+        {
+            return value;
+        }
+        public string getCardName()
+        {
+            return cardName;
+        }
+        public string getCardRole()
+        {
+            return role;
+        }
+        public string getCardDescription()
+        {
+            return description;
+        }
+
+        public virtual void pickUp(){}
+        public virtual void play(Player accuser, Player accused) {}
+        public virtual void discard(){}
+    }
+
+    public class GuardCard:Card
+    {
+        public GuardCard(string givenName)
+        {
+            value = 1;
+            role = "Guard";
+            cardName = givenName;
+        }
+        public override void play(Player accuser, Player accused){
+            //Player accused = null; //replace with the accused player
+            //int guessValue = 0; // replace with guessed card
+            //if (guessvalue == accused.getCard().getCardValue()){
+                //accused.takeOut();
+            //}
+        }
+    }
+    
+    public class PriestCard:Card
+    {
+        public PriestCard(string givenName)
+        {
+            value = 2;
+            role = "Priest";
+            cardName = givenName;
+        }
+        public override void play(Player accuser, Player accused){
+            //Player confessed = null; // replace with the targeted player.
+            // tell the player confessed.getCard().getValue() + confessed.getCard().getRole()
+        }
+    }
+    
+    public class BaronCard:Card
+    {
+        public BaronCard(string givenName)
+        {
+            value = 3;
+            role = "Baron";
+            cardName = givenName;
+        }
+        public override void play(Player accuser, Player accused)
+        {
+            if (accuser.getCardFromHand().getValue() > accused.getCardFromHand().getValue())
+            {
+                accused.takeOut();
+            }
+            else if (accuser.getCardFromHand().getValue() < accused.getCardFromHand().getValue())
+            {
+                accuser.takeOut();
+            }
+            else
+            {
+                //tell players that they have the same card and no-one goes out
+            }
+        }
+    }
+    
+    public class HandmaidCard:Card
+    {
+        public HandmaidCard(string givenName)
+        {
+            value = 4;
+            role = "Handmaid";
+            cardName = givenName;
+        }
+        public override void play(Player accuser, Player accused)
+        {
+            //Make player protected for 1 round
+        }
+    }
+    
+    public class PrinceCard:Card
+    {
+        public PrinceCard(string givenName)
+        {
+            value = 5;
+            role = "Prince";
+            cardName = givenName;
+        }
+        public override void play(Player accuser, Player accused)
+        {
+            accused.takeOut();
+        }
+    }
+    
+    public class KingCard:Card
+    {
+        public KingCard(string givenName)
+        {
+            value = 6;
+            role = "King";
+            cardName = givenName;
+        }
+        public override void play(Player accuser, Player accused)
+        {
+        }
+    }
+    
+    public class CountessCard:Card
+    {
+        public CountessCard(string givenName)
+        {
+            value = 7;
+            role = "Countess";
+            cardName = givenName;
+        }
+    }
+    
+    public class PrincessCard:Card
+    {
+        public PrincessCard(string givenName)
+        {
+            value = 8;
+            role = "Princess";
+            cardName = givenName;
+        }
+    }
+
+    public class GroupedCards
+    {
+        private List<Card> cards;
+        public GroupedCards()
+        {
+            cards = new List<Card>();
+        }
+        public Card getCard(int ind)
+        {
+            return cards[ind];
+        }
+        public void addCard(Card cardToAdd)
+        {
+            cards.Add(cardToAdd);
+        }
+        public Card takeCard(int indexTaken)
+        {
+            Card cardTaken = cards[indexTaken];
+            cards.RemoveAt(indexTaken);
+            return cardTaken;
+        }
+        public int getCount()
+        {
+            return cards.Count;
+        }
+    }
+
+    public class Player
+    {
+        private int playerNumber;
+        private string playerName;
+        private GroupedCards heldCards;
+        private GroupedCards playedCards;
+
+        public Player(int num, string name)
+        {
+            playerNumber = num;
+            playerName = name;
+        }
+        public Card getCardFromHand()
+        {
+            return heldCards.getCard(0);
+        }
+        public string getPlayerName()
+        {
+            return playerName;
+        }
+        public void takeOut()
+        {
+            heldCards.getCard(0).discard();
+        }
+    }
+}
